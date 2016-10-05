377812d1b5715406315fcf3481f3253f6dee9eed
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-10-05 05:18:02 -0300

    Added images

--- build/bundle.js
+++ build/bundle.js
@@ -39880,19 +39880,19 @@
 	  value: true
 	});
 	exports.default = [{
-	  image: "http://placehold.it/256x192/111111/ffffff",
+	  image: "images/monocular.png",
 	  name: "Monocular Rift",
 	  description: "Game made for Ludum Dare 36. Explores mechanic\n      of apparent position of objects when viewed with zoom. ",
 	  rating: 5,
 	  github: "https://github.com/Ghust1995/ld36"
 	}, {
-	  image: "http://placehold.it/256x192/111111/ffffff",
+	  image: "images/ddd1440.jpg",
 	  name: "DDD 1440",
 	  description: "Game made for Ludum Dare 35. Multiplayer sumo brawler with nice\n      minimalist aesthetics and simple yet deep mechanics",
 	  rating: 4,
 	  github: "https://github.com/Ghust1995/LD35Shapeshift"
 	}, {
-	  image: "http://placehold.it/256x192/111111/ffffff",
+	  image: "images/myengine.png",
 	  name: "My Engine",
 	  description: "Game Programming framework built with THREE.js, supporting\n      multiplayer and a simple real time editor. ",
 	  rating: 5,
--- data/projects.js
+++ data/projects.js
@@ -1,6 +1,6 @@
 export default [
   {
-    image: `http://placehold.it/256x192/111111/ffffff`,
+    image: `images/monocular.png`,
     name: "Monocular Rift",
     description: `Game made for Ludum Dare 36. Explores mechanic
       of apparent position of objects when viewed with zoom. `,
@@ -8,7 +8,7 @@ export default [
     github: "https://github.com/Ghust1995/ld36"
   },
   {
-    image: `http://placehold.it/256x192/111111/ffffff`,
+    image: `images/ddd1440.jpg`,
     name: "DDD 1440",
     description: `Game made for Ludum Dare 35. Multiplayer sumo brawler with nice
       minimalist aesthetics and simple yet deep mechanics`,
@@ -16,7 +16,7 @@ export default [
     github: "https://github.com/Ghust1995/LD35Shapeshift"
   },
   {
-    image: `http://placehold.it/256x192/111111/ffffff`,
+    image: `images/myengine.png`,
     name: "My Engine",
     description: `Game Programming framework built with THREE.js, supporting
       multiplayer and a simple real time editor. `,
